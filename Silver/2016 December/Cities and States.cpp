5d68ddd256953a2a5524d70c93ea49ae33914209
Tanbin Hasan Ador <tanvin.hasan74@gmail.com>
2021-07-08 21:41:37 +0600

    Update Cities and States.cpp

--- Silver/2016 December/Cities and States.cpp
+++ Silver/2016 December/Cities and States.cpp
@@ -13,7 +13,6 @@ int main(void) {
   freopen("citystate.in" , "r" , stdin) ;
   freopen("citystate.out" , "w" , stdout) ;
   map<pair<string,string>,int64_t> cnt ;
-  map<pair<string,string>,bool> mark ;
   int64_t n ;
   cin >> n ;
   while (n--) {
@@ -24,13 +23,9 @@ int main(void) {
   int64_t ans = 0 ;
   for (auto &[x , y] : cnt) {
     auto &[a , b] = x ;
-    if (mark[x] || mark[{b , a}]) {
-      continue ;
-    }
-    if (a != b) {
+    if (a < b) {
       ans += y * cnt[{b , a}] ;
     }
-    mark[x] = true ;
   }
   cout << ans ;
   return 0 ;
